9e971bf4e551c0bdba62e7eb93314ff487962d0f
vinsssten <dan.wall@mail.ru>
2021-09-20 16:01:09 +0700

    Добавьте файлы проекта.

---
+++ Session1_01/Add_user.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Session1_01
+{
+    /// <summary>
+    /// Логика взаимодействия для Add_user.xaml
+    /// </summary>
+    public partial class Add_user : Window
+    {
+        public Add_user()
+        {
+            InitializeComponent();
+        }
+    }
+}
